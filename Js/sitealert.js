80628f3433948726d218cdce843dae775367722b
Neal <darc@localhost.localdomain>
2020-01-18 00:23:18 +0300

    This is my simple Portflio Website. Coded by Me.

---
+++ Js/sitealert.js
@@ -0,0 +1,3 @@
+function notCompleted(){
+	alert("Hello,Am SImon. Welcome to my Portfolio.\n The site is still under Development, some of the buttons may not work.\nIt might also not work well on some smartphone browsers' but will make it resposive enough for all devices.Thank You !!")
+}
